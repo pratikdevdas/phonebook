d772e2816b2c2dcca51db05e70c72804f86e6633
rev <dasdev.pratik@gmail.com>
2021-12-24 21:14:28 +0530

    fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 require('express-async-errors')
-// const cors = require('cors')
+const cors = require('cors')
 const personsRouter = require('./controllers/persons')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
@@ -20,16 +20,16 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connecting to MongoDB:', error.message)
   })
 
-// app.use(cors())
+app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-// app.use(middleware.requestLogger)
+app.use(middleware.requestLogger)
 
 app.use('/api/persons', personsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
-// app.use(middleware.unknownEndpoint)
+app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
 module.exports = app
--- src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-const Filter = ({prop,prop2}) =>{return (<>
-    <input
-      type="text"
-      placeholder="Search"
-      value={prop}
-      onChange={prop2}
-    /></>)}
-export default Filter
